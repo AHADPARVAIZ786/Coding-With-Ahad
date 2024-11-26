d48476dc3edfc373ac36af6020f5548907d0b668
CYBER WORLD <islamicguidanve786.0@gmail.com>
2024-11-26 14:48:38 -0800

    Sum of two Numbers with int data type
    Sum of two Numbers
    It is a practice program
    It is valid for only int data type

---
+++ Sum of two numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int num_1, num_2, sum;
+
+    printf("Enter 1st number :");
+    scanf("%d", &num_1);
+
+    printf("Enter 2nd number :");
+    scanf("%d", &num_2);
+
+    sum = num_1 + num_2;
+
+    printf("Sum is = %d", sum);
+}
