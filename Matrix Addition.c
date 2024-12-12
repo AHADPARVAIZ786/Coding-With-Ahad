86ccb2a083382795c95e2b6b84bbdd8227a133fd
CYBER WORLD <islamicguidanve786.0@gmail.com>
2024-12-12 22:38:29 +0500

    Update Matrix Addition.c

--- Matrix Addition.c
+++ Matrix Addition.c
@@ -3,43 +3,20 @@
 
 void main()
 {
-    int a[3][3], b[3][3], c[3][3], i, j;
-
-    printf("\n Enter the Value of 1st Matrix:\n");
-    for (i=0; i<3; i++)
-    {
-        for (j=0; j<3; j++)
-        {
-            printf("\nEnter the Value of [%d][%d] =", i, j);
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    printf("\n Enter the Value of 2nd Matrix:\n");
-    for (i=0; i<3; i++)
-    {
-        for (j=0; j<3; j++)
-        {
-            printf("\nEnter the Value of [%d][%d] =", i, j);
-            scanf("%d", &b[i][j]);
-        }
-    }
+    int a[2][2]={{1,2},{3,4}};
+    int b[2][2]={{5,6},{7,8}};
+    int c[2][2];
+    int i, j;
 
     printf("\nSum of Matrix is:\n");
-    for (i=0; i<3; i++)
+    for (i=0; i<2; i++)
     {
-        for (j=0; j<3; j++)
+        for (j=0; j<2; j++)
         {
             c[i][j] = a[i][j] + b[i][j];
-        }
-    }
-
-    for (i=0; i<3; i++)
-    {
-        for (j=0; j<3; j++)
-        {
             printf("%d \t", c[i][j]);
         }
         printf("\n");
     }
+
 }
